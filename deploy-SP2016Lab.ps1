244c8e64bb2b9f5789ecf1c657222a0a16953e10
Rob Garrett <rob@robgarrett.com>
2018-08-17 18:37:48 -0400

    Added windows timezone tweak

--- deploy-SP2016Lab.ps1
+++ deploy-SP2016Lab.ps1
@@ -1,8 +1,20 @@
 
 [CmdletBinding()]Param()
 
+# Create my own storage account.
+$labName = "Lab-SP2016";
+$ResourceGroupName = "$($labName)-Artifacts";
+$StorageAccountName = $ResourceGroupName.ToLower().Replace("-", "");
+$Location = "CentralUS";
+
+New-AzureRmResourceGroup -Location $Location -Name $ResourceGroupName -Force;
+New-AzureRmStorageAccount -StorageAccountName $StorageAccountName -Type 'Standard_LRS' -ResourceGroupName $ResourceGroupName -Location $Location;
+
+# Deploying.
 ./deployAzureTemplate.ps1 `
-    -ArtifactStagingDirectory "Lab-SP2016" `
+    -StorageAccountName $StorageAccountName
+    -ArtifactStagingDirectory "Common\AD" `
     -ResourceGroupLocation "CentralUS" `
     -ResourceGroupName "Lab-SP2016" `
-    -UploadArtifacts
+    -TemplateParametersFile "Lab-SP2016\azuredeploy.parameters.json"
+    -UploadArtifacts;
--- deploy-SP2016Lab.sh
+++ deploy-SP2016Lab.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -e
+
+labName="Lab-SP2016"
+resourceGroupName="${labName}-Artifacts"
+storageAccountName=$( echo "${resourceGroupName//-/}" | awk '{print tolower($0)}' )
+
+# Create my own storage account.
+az group create -n "${resourceGroupName}" -l "CentralUS"
+az storage account create -l "CentralUS" --sku "Standard_LRS" -g "${resourceGroupName}" -n "${storageAccountName}"
+
 # Need availability zones, which are currently in Cemntral US.
-./deployAzureTemplate.sh -a "Common/AD" -g Lab-SP2016 -l CentralUS -e "Lab-SP2016/azuredeploy.parameters.json"
+./deployAzureTemplate.sh -a "Common/AD" -g Lab-SP2016 -l CentralUS -e "Lab-SP2016/azuredeploy.parameters.json" -s "${storageAccountName}"
