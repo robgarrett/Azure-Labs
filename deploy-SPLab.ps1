80576e4468f7a80b4b3934b2b94e82abc66c95fe
Rob Garrett <rob@robgarrett.com>
2018-08-23 14:01:26 -0400

    Added SP Servers with domain join

--- deploy-SPLab.ps1
+++ deploy-SPLab.ps1
@@ -4,16 +4,35 @@
 # Create my own storage account.
 $ResourceGroupName = "$($labName)-Artifacts";
 $StorageAccountName = $ResourceGroupName.ToLower().Replace("-", "");
+$StorageContainerName = "$($ResourceGroupName.ToLower())-stageartifacts";
 $Location = "CentralUS";
 
 New-AzureRmResourceGroup -Location $Location -Name $ResourceGroupName -Force;
 New-AzureRmStorageAccount -StorageAccountName $StorageAccountName -Type 'Standard_LRS' -ResourceGroupName $ResourceGroupName -Location $Location;
 
-# Deploying.
+Function EmptyContainer {
+    $StorageAccount = (Get-AzureRmStorageAccount | Where-Object{$_.StorageAccountName -eq $StorageAccountName});
+    $context = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccount.Context;
+    Get-AzureStorageBlob -Container $StorageContainerName -blob * -Context $context | % {
+        Remove-AzureStorageBlob -Blob $_.Name -Container $StorageContainerName -Context $context;
+    }
+}
+
+# Deploy AD.
 ./deployAzureTemplate.ps1 `
     -StorageAccountName $StorageAccountName
     -ArtifactStagingDirectory "Common\AD" `
     -ResourceGroupLocation "CentralUS" `
     -ResourceGroupName $labName `
-    -TemplateParametersFile "$labName\azuredeploy.parameters.json"
+    -TemplateParametersFile "$labName\AD\azuredeploy.parameters.json"
+    -UploadArtifacts;
+
+# Deploy SP.
+EmptyContainer;
+./deployAzureTemplate.ps1 `
+    -StorageAccountName $StorageAccountName
+    -ArtifactStagingDirectory "Common\SP" `
+    -ResourceGroupLocation "CentralUS" `
+    -ResourceGroupName $labName `
+    -TemplateParametersFile "$labName\SP\azuredeploy.parameters.json"
     -UploadArtifacts;
--- deploy-SPLab.sh
+++ deploy-SPLab.sh
@@ -15,10 +15,23 @@ fi
 echo "Creating ${labName}"
 resourceGroupName="${labName}-Artifacts"
 storageAccountName=$( echo "${resourceGroupName//-/}" | awk '{print tolower($0)}' )
+storageContainerName=$( echo "${labName}-stageartifacts" | awk '{print tolower($0)}' )
+
+# Check for existing VMs and start them.
+VM_NAMES=$(az vm list -g $resourceGroupName --show-details --query "[?powerState=='VM deallocated'].{ name: name }" -o tsv)
+for NAME in $VM_NAMES
+do
+    echo "Starting VM $NAME"
+    az vm start -n $NAME -g $resourceGroupName --no-wait
+done
 
 # Create my own storage account.
 az group create -n "${resourceGroupName}" -l "CentralUS"
 az storage account create -l "CentralUS" --sku "Standard_LRS" -g "${resourceGroupName}" -n "${storageAccountName}"
 
-# Need availability zones, which are currently in Cemntral US.
-./deployAzureTemplate.sh -a "Common/AD" -g "${labName}" -l CentralUS -e "${labName}/azuredeploy.parameters.json" -s "${storageAccountName}"
+# Need availability zones, which are currently in Central US.
+#./deployAzureTemplate.sh -a "Common/AD" -g "${labName}" -l CentralUS -e "${labName}/AD/azuredeploy.parameters.json" -s "${storageAccountName}"
+
+# Create the SP Servers.
+az storage blob delete-batch --account-name "${storageAccountName}" --source "${storageContainerName}"
+./deployAzureTemplate.sh -a "Common/SP" -g "${labName}" -l CentralUS -e "${labName}/SP/azuredeploy.parameters.json" -s "${storageAccountName}"
